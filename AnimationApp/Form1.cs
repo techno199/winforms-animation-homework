f5bb6adc25ba91ee0ad7c54250151d78d9fc017b
ksan199 <techno199@live.ru>
2018-11-16 19:18:31 +0300

    Добавьте файлы проекта.

---
+++ AnimationApp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace AnimationApp
+{
+    public partial class Form1 : Form
+    {
+        // Размерность графа
+        public const int GraphWidth = 10;
+        public const int GraphHeight = 4;
+
+        // Размеры фигуры в пикселях
+        public const int RectangleWidth = 100;
+        public const int RectangleHeight = 100;
+
+        // Количество пикселей на шаг графа
+        public double PixelMultiplierX { get; set; }
+        public double PixelMultiplierY { get; set; }
+
+        // Первоначальные координаты фигуры внутри графа
+        public double currentX { get; set; } = 0;
+        public double currentY { get; set; } = 0;
+
+        // Сдвиг оси координат в писклях внутри формы
+        public int ShiftX { get; set; } = 0;
+        public int ShiftY { get; set; } = 0;
+
+        // Шаг отрисовки по оси X
+        public double Step { get; set; } = 0.05;
+
+        public System.Timers.Timer Timer { get; set; } = new System.Timers.Timer();
+        public Graphics Graphics { get; set; }
+        public Pen Pen { get; set; } = new Pen(Color.FromArgb(255, 0, 0, 0));
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Вычисляем шаг изменения координат в пикселях относительно размеров формы
+            PixelMultiplierX= Width / GraphWidth;
+            PixelMultiplierY = Height / GraphHeight;
+
+            // Смещаем граф, чтобы фигура оказалась посередине формы
+            ShiftY = Height / 2 - RectangleHeight / 2;
+
+            // Получаем ссылку на графику формы
+            Graphics = CreateGraphics();
+
+            // Задаем таймер
+            Timer.Elapsed += (sender, e) => OnTimedEvent();
+            Timer.Interval = 10;
+            Timer.Enabled = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+        private void OnTimedEvent()
+        {
+            // Считаем новые координаты 
+            currentX += Step;
+            currentY = Math.Sin(currentX);
+
+            // Округляем результаты X по модулю размерности координатной доски, 
+            // чтобы фигура отрисовывалась в цикле и не уплывала за пределы формы
+            currentX %=  GraphWidth;
+
+            DrawRectangle();
+        }
+
+
+        private void DrawRectangle()
+        {
+            var g = CreateGraphics();
+            g.Clear(Color.White);
+            g.DrawRectangle(Pen, new Rectangle(
+                Convert.ToInt32(currentX * PixelMultiplierX + ShiftX), 
+                Convert.ToInt32(currentY * PixelMultiplierY + ShiftY),
+                RectangleWidth,
+                RectangleHeight
+            ));
+        }
+    }
+}
